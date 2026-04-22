e8dc5ac7a460bd5291b5e7684570a4c75d92995d
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-04-22 08:24:53 +0530

    Add files via upload

---
+++ slidingwindow.java
@@ -0,0 +1,40 @@
+// public class slidingwindow {
+    
+// }
+
+
+
+
+
+
+
+
+// class slidingWindow{
+//     public static void main(String[]args){
+//         int arr[]={1,2,3,4,5};
+//         int k=3;
+//         int n=arr.length;
+//         int maxSum=0;
+//         for(int i=0;i<k;i++){
+//             maxSum+=arr[i];
+//         }
+//         int windowSum=maxSum;
+//         for(int i=k;i<n;i++){
+//             windowSum+=arr[i]-arr[i-k];
+//             maxSum=Math.max(maxSum,windowSum);
+//         }
+//         System.out.println("The maximum sum of "+k+" consecutive elements is: "+maxSum);
+//     }    
+            
+// }
+
+
+
+
+
+
+
+
+
+
+
