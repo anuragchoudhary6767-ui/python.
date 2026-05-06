fe3b76f7ef92f7918751e458299c34d683876dd6
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-05-06 10:13:02 +0530

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,13 @@
+# Existing dictionary
+student_grades = {
+    'Alice': 85,
+    'Bob': 90,
+    'Charlie': 78,
+    'David': 92
+}
+
+# Update Charlie's grade to 80
+student_grades['Charlie'] = 80
+
+# Print updated dictionary
+print(student_grades)
