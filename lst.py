956cb7e8a86cb4de5367488e6634c1df12606507
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-09-14 09:37:03 +0530

    Add files via upload

---
+++ lst.py
@@ -0,0 +1,17 @@
+lst={7,-4,-2,9,0,11,17,8,19,10}
+# print(list[1::4])
+# lst.append("hello")
+# lst.insert(-4,36)
+lst1=[45,46,47]
+lst.extend(lst1)
+lst+=lst1#extend
+lst+=lst1
+print (lst)
+
+
+
+
+
+lst=[5,3,2,1,4,8]
+list()
+lst=[18,20,21,22,19,15]
